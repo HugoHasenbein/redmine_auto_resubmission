25a0c739008173e6428749f4a3f2cac55369bef4
Dr. Stephan Wenzel <stephan.wenzel@drwpatent.de>
2018-07-09 17:22:39 +0200

    code cosmetics

--- lib/redmine_auto_resubmission.rb
+++ lib/redmine_auto_resubmission.rb
@@ -19,7 +19,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #
 
-.configuration.to_prepare do
+Rails.configuration.to_prepare do
 
   # main library
   require 'redmine_auto_resubmission/redmine_auto_resubmission_lib'
