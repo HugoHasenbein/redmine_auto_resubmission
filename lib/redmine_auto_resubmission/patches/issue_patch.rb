64e2cfd008836663bb7f721a419d3a100b860e4e
Dr. Stephan Wenzel <stephan.wenzel@drwpatent.de>
2020-01-17 11:23:13 +0100

    catch misconfiguration errors

--- lib/redmine_auto_resubmission/patches/issue_patch.rb
+++ lib/redmine_auto_resubmission/patches/issue_patch.rb
@@ -59,7 +59,7 @@ module RedmineAutoResubmissionIssuePatch
           ##########################################################
           if resubmission_rule.present?
           
-            new_date, new_rule = RedmineAutoResubmission::calcfuturedate( resubmission_date.value, resubmission_rule.value )
+            new_date, new_rule = RedmineAutoResubmission::calcfuturedate( resubmission_date && resubmission_date.value, resubmission_rule.value )
             resubmission_rule.value= new_rule                      if new_rule
             resubmission_date.value= new_date.strftime("%Y-%m-%d") if new_date
             @custom_field_values_changed= true                     if new_rule || new_date
